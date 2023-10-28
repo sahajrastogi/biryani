fba05d342698189e9b676940cdc904566e527bfb
Shashwat Raj <rajayshashwat@gmail.com>
2023-10-28 11:40:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import navbar from './navbar'
+
+function App() {
+  const title = "Ferrum Sigma";
+  
+  return (
+    <div className="App">
+      <div className="content"></div>
+        <h2>{ title }</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+const navbar = () => {
+    return ( 
+        <nav classname="navbar">
+            <h1>
+                Biryani
+            </h1>
+            <div classname="links">
+                <a href="/">Home</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default navbar;
